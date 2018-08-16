fe2658d788c549ce4491e614f363058e12206b81
Julian Kingman <julian@designman.io>
2018-08-16 02:45:45 -0700

    fix

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ ynabChannel.handlers.user_info = function(request, callback) {
     })
     .then(function(data) {
       console.log(data);
-      callback(null, { id: data.data.user.id, name: 'ynab-contest' });
+      callback(data.error, { id: data.data.user.id, name: 'ynab-contest' });
     });
 };
 
