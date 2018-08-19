8494f7000d03b7ad986b2a755158a846a9774685
Julian Kingman <julian@designman.io>
2018-08-19 13:33:49 -0700

    fix account field opts

--- fields/getAccountFieldOptions.js
+++ fields/getAccountFieldOptions.js
@@ -8,10 +8,16 @@ const getAccountFieldOptions = (_getOptionsData = function(req, response, cb) {
   var api = ynabApi(req);
 
   api.accounts.getAccounts('last-used').then(function(data) {
-    var accounts = data.data.accounts;
+    let accounts = data.data.accounts.sort(
+      (a1, a2) => (a1.name < a2.name ? -1 : a2.name < a1.name ? 1 : 0)
+    );
+    let falsey = new response.OptionEntity();
+    falsey.setLabel('None');
+    falsey.setValue('none');
+    response.addOption(falsey);
     accounts.forEach(account => {
       // console.log('account', account);
-      const option = new response.OptionEntity();
+      option = new response.OptionEntity();
       option.setLabel(account.name);
       option.setValue(account.id);
       response.addOption(option);
