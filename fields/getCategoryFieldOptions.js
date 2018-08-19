8d837f67e22fa03c3b25434e3a50a178ce3a2eda
Julian Kingman <julian@designman.io>
2018-08-19 04:59:39 -0700

    .

--- fields/getCategoryFieldOptions.js
+++ fields/getCategoryFieldOptions.js
@@ -2,13 +2,20 @@ var ynabApi = require('../ynabApi');
 
 const getCategoryFieldOptions = async (req, response, cb) => {
   var api = ynabApi(req);
-  let categories = await api.categories.getCategories('last-used');
+  let categoryGroups = await api.categories.getCategories('last-used');
+  categoryGroups = categoryGroups.data.category_groups;
+  const categories = categoryGroups.map(group => ({
+    label: group.name,
+    values: group.categories.map(category => ({
+      name: category.name,
+      value: category.id,
+    })),
+  }));
   console.log('categories', categories);
-  categories = categories.data.categories;
   categories.forEach(category => {
     const option = new response.OptionEntity();
-    option.setLabel(category.name);
-    option.setValue(category.id);
+    option.setLabel(category.label);
+    option.setValue(category.values);
     response.addOption(option);
   });
   cb(null);
